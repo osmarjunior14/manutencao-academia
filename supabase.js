56efa167ec0e0731868424246cbe5d9fb2e0674d
duoacademia <osmar.coordenacaoduo@gmail.com>
2026-03-16 10:54:51 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://chyomxetiloktyepjbtk.supabase.co"
+const SUPABASE_KEY = "sb_publishable_ZuuKIqmGotY08ZepHonxHg_3IOjs6pI"
+
+const supabase = window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+)
